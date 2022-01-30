26020e470d303c3cd76fd803c051b5829d174251
luis <luislopez1099lg@gmail.com>
2022-01-29 21:27:21 -0700

    refactor(Root): code styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,68 +1,68 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import { useFetch } from "../hooks/useFetch";
-import { ExerciseItem } from "../components/homepage/ExerciseItem";
-import { ToolBarSearch } from "../components/searchBar/ToolBarSearch";
-import { Wrapper } from "../components/shareStyleComponents/Wrapper";
-import { toast } from "react-toastify";
-import { endpoints } from "../components/signIn/api";
-import { NoResultsMessage } from "../components/homepage/NoResultsMessage";
-import { getArrayBySize } from "../helpers/getArrayBySize";
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { useFetch } from '../hooks/useFetch'
+import { ExerciseItem } from '../components/homepage/ExerciseItem'
+import { ToolBarSearch } from '../components/searchBar/ToolBarSearch'
+import { Wrapper } from '../components/shareStyleComponents/Wrapper'
+import { toast } from 'react-toastify'
+import { endpoints } from '../components/signIn/api'
+import { NoResultsMessage } from '../components/homepage/NoResultsMessage'
+import { getArrayBySize } from '../helpers/getArrayBySize'
 
-const initialExercises = null;
+const initialExercises = null
 
 export const HomePage = () => {
-  const [exercises, setExercises] = useState(initialExercises);
-  const { fetchData, fetchErrors, loading, data } = useFetch();
-  const [category, setCategory] = useState(null);
-  const [search, setSearch] = useState("");
+  const [exercises, setExercises] = useState(initialExercises)
+  const { fetchData, fetchErrors, loading, data } = useFetch()
+  const [category, setCategory] = useState(null)
+  const [search, setSearch] = useState('')
 
   const selectCategory = (idCategory) => {
-    setCategory(idCategory);
-  };
+    setCategory(idCategory)
+  }
 
   const setSearchQuery = (query) => {
-    setSearch(query);
-  };
+    setSearch(query)
+  }
 
   const searchByQuery = () => {
-    if (search === "" && category === null) return 0;
-    if (category !== null && category !== -1 && search !== "") {
-      let url = `${endpoints.search}?query=${search}&category=${category}`;
-      fetchData(url);
+    if (search === '' && category === null) return 0
+    if (category !== null && category !== -1 && search !== '') {
+      let url = `${endpoints.search}?query=${search}&category=${category}`
+      fetchData(url)
     } else if (category !== null && category !== -1) {
-      let url = `${endpoints.search}?category=${category}`;
-      fetchData(url);
-    } else if (search !== "") {
-      let url = `${endpoints.search}?query=${search}`;
-      fetchData(url);
+      let url = `${endpoints.search}?category=${category}`
+      fetchData(url)
+    } else if (search !== '') {
+      let url = `${endpoints.search}?query=${search}`
+      fetchData(url)
     }
-  };
+  }
 
   useEffect(() => {
-    fetchData(endpoints.exercises);
-  }, []);
+    fetchData(endpoints.exercises)
+  }, [])
 
   useEffect(() => {
-    searchByQuery();
-  }, [category]);
+    searchByQuery()
+  }, [category])
 
   useEffect(() => {
-    if (data === null) return null;
-    setExercises(filterEnableExer(data.data));
-  }, [data]);
+    if (data === null) return null
+    setExercises(filterEnableExer(data.data))
+  }, [data])
 
   const filterEnableExer = (items) => {
-    return items.filter((item) => item.status !== 2);
-  };
+    return items.filter((item) => item.status !== 2)
+  }
 
   useEffect(() => {
     if (fetchErrors) {
-      toast.error("Algo salió mal :(");
+      toast.error('Algo salió mal :(')
     }
-  }, [fetchErrors]);
+  }, [fetchErrors])
 
-  const skeletons = getArrayBySize({ size: 3 });
+  const skeletons = getArrayBySize({ size: 6 })
 
   return (
     <HomeContainer>
@@ -95,13 +95,13 @@ export const HomePage = () => {
         </QuotesContainer>
       )}
     </HomeContainer>
-  );
-};
+  )
+}
 
-const HomeContainer = styled(Wrapper)``;
+const HomeContainer = styled(Wrapper)``
 
 const QuotesContainer = styled.main`
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   gap: 2rem;
-`;
+`
