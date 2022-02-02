8520c9dbdb341eea81f10353a1c05235ae68ad75
luis <luislopez1099lg@gmail.com>
2022-02-01 23:26:59 -0700

    fix(Root): color errrors

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components'
-import { Wrapper } from '../components/shareStyleComponents/Wrapper'
-
 import notFound from '../images/notFoundPage.svg'
 import { Button } from '../components/ui/Button'
 import { useLinkRouter } from '../hooks/useLinkRouter'
+import { FlexContainer } from '../components/shareStyleComponents/FlexContainer'
 
 export const NotFoundPage = () => {
   const { goHomePage } = useLinkRouter()
@@ -13,7 +12,7 @@ export const NotFoundPage = () => {
   }
 
   return (
-    <Container flex flex_dc flex_jc_c flex_ai_c>
+    <Container fd_c jc_c ai_c gap="1rem">
       <Image src={notFound} />
       <p>Vaya, no hemos podido encontrar lo que buscas.</p>
       <Button primary={true} onClick={handleClick}>
@@ -23,8 +22,9 @@ export const NotFoundPage = () => {
   )
 }
 
-const Container = styled(Wrapper)`
+const Container = styled(FlexContainer)`
   margin: 0 auto;
+  color:${({ theme: { fontColor } }) => fontColor};
 `
 
 const Image = styled.img``
