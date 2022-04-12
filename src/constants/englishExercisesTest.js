5cc2e831ff8bb54b7b1e4364ae2648639273c08f
luis <luislopez1099lg@gmail.com>
2022-04-12 15:03:26 -0600

    feat(Root): new testing dat

--- src/constants/englishExercisesTest.js
+++ src/constants/englishExercisesTest.js
@@ -1,38 +1,64 @@
 export const EnglishExercisesMockup = [
   {
     id: 11231,
-    categoryName: 'Verb to be',
+    categoryName: 'Comida',
     exercises: [
       {
         id: 11,
         name: '1',
-        title: '¿Cuál es la traducción de apple?',
+        title: '¿Cuál de estos es "Café"?',
+        isDone: false,
         answers: [
-          { id: 112, content: 'manzana', isCorrect: true },
-          { id: 113, content: 'Pera', isCorrect: false },
-          { id: 114, content: 'Platano', isCorrect: false }
+          { id: 112, content: 'Coffe', isCorrect: true },
+          { id: 113, content: 'Sugar', isCorrect: false },
+          { id: 114, content: 'Tea', isCorrect: false }
         ]
       },
       {
         id: 12,
         name: '2',
-        title: '¿Cuál es la traducción de pear?',
+        title: '¿Cómo se dice "té"?',
+        isDone: true,
         answers: [
-          { id: 122, content: 'manzana', isCorrect: false },
-          { id: 123, content: 'Pera', isCorrect: true },
-          { id: 124, content: 'Platano', isCorrect: false }
+          { id: 122, content: 'Tea', isCorrect: true },
+          { id: 123, content: 'Hello', isCorrect: false },
+          { id: 124, content: 'Coffe', isCorrect: false }
+        ]
+      },
+
+      {
+        id: 13,
+        name: '3',
+        title: '¿Cómo se dice "Hola"?',
+        isDone: true,
+        answers: [
+          { id: 122, content: 'Hello', isCorrect: true },
+          { id: 123, content: 'Bye', isCorrect: false },
+          { id: 124, content: 'Nice', isCorrect: false }
+        ]
+      },
+      {
+        id: 14,
+        name: '4',
+        title: '¿Cómo se dice "Por favor"?',
+        isDone: false,
+        answers: [
+          { id: 122, content: 'Please', isCorrect: true },
+          { id: 123, content: 'Fast', isCorrect: false },
+          { id: 124, content: 'Come on', isCorrect: false }
         ]
       }
     ]
   },
   {
     id: 21212,
-    categoryName: 'Vocabulario',
+    categoryName: 'House',
     exercises: [
       {
         id: 21,
         name: '1',
         title: '¿Cuál es la traducción de apple?',
+        isDone: true,
         answers: [
           { id: 211, content: 'manzana', isCorrect: true },
           { id: 212, content: 'Pera', isCorrect: false },
@@ -43,6 +69,7 @@ export const EnglishExercisesMockup = [
         id: 22,
         name: '2',
         title: '¿Cuál es la traducción de pear?',
+        isDone: false,
         answers: [
           { id: 221, content: 'manzana', isCorrect: false },
           { id: 222, content: 'Pera', isCorrect: true },
