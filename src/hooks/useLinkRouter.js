f3205b76fbcea53f7f95ec2331bc4e56e1ae0d87
luis <luislopez1099lg@gmail.com>
2022-04-13 17:53:40 -0600

    feat(Root): route updated

--- src/hooks/useLinkRouter.js
+++ src/hooks/useLinkRouter.js
@@ -4,7 +4,7 @@ import { routesV2, routesV3 } from '../routes'
 export const useLinkRouter = () => {
   const navigate = useNavigate()
 
-  const goHomePage = () => navigate(routesV2.LOGGED_APP.subPages.HOME_PAGE.route)
+  const goHomePage = () => navigate(routesV3.MECA_TIME_PAGE.route)
   const goToConfigProfilePage = () => navigate(routesV2.LOGGED_APP.subPages.CONFIG_PROFILE_PAGE.route)
   const goLoginPage = () => navigate(routesV3.LOGIN_PAGE.route)
   const goSignUpPage = () => navigate(routesV3.SIGNUP_PAGE.route)
