61da030d743ce617d7fb69dbebf6482e95a67e5b
luis <luislopez1099lg@gmail.com>
2022-06-06 07:35:19 -0600

    feat(Root): icons added

--- src/pages/EnglishPage.js
+++ src/pages/EnglishPage.js
@@ -5,35 +5,42 @@ import { FlexContainer } from '../components/shareStyleComponents/FlexContainer'
 import { useSession } from '../hooks/useSession'
 import { Layout } from '../layouts/Layout'
 import { routesV3 } from '../routes'
+import { IoBarChart, IoKeySharp, IoHomeSharp } from 'react-icons/io5'
 
 const EnglishPage = () => {
   const { isAdmin } = useSession()
 
   return (
     <Layout porRe={true}>
-
       <FlexContainer>
-        <FlexContainer fd_c pd='1rem' gap='1rem'>
+        <FlexContainer fd_c gap='1rem'>
           <StyledLink
             to={routesV3.ENGLISH_PAGE.route}
             className={({ isActive }) => (isActive ? 'activated' : null)}>
-            Home
+            <FlexContainer gap="0.5rem" jc_fs ai_c>
+              <IoHomeSharp /> Inicio
+            </FlexContainer>
           </StyledLink>
 
           <StyledLink
             to={routesV3.ENGLISH_PAGE.subRoutes.ENGLISH_STATS_PAGE.route}
             className={({ isActive }) => (isActive ? 'activated' : null)}>
-            Stats
+            <FlexContainer gap="0.5rem" jc_fs ai_c>
+              <IoBarChart />
+              Estadísticas
+            </FlexContainer>
           </StyledLink>
 
           {isAdmin() && (
             <StyledLink
               to={routesV3.ENGLISH_PAGE.subRoutes.ENGLISH_ADMIN_PAGE.route}
               className={({ isActive }) => (isActive ? 'activated' : null)}>
-              Admin
+              <FlexContainer gap="0.5rem" jc_c ai_c>
+                <IoKeySharp />
+                Admin
+              </FlexContainer>
             </StyledLink>
           )}
-
         </FlexContainer>
         <Layout width='100%'>
           <Outlet />
--- src/pages/RicePage.js
+++ src/pages/RicePage.js
@@ -4,24 +4,43 @@ import { Button } from '../components/ui/Button'
 import { useSession } from '../hooks/useSession'
 import { Layout } from '../layouts/Layout'
 import { routesV3 } from '../routes'
+import { IoKeySharp, IoHomeSharp } from 'react-icons/io5'
+import styled from 'styled-components'
 
 export const RicePage = () => {
   const { isAdmin } = useSession()
   return (
     <FlexContainer gap='2rem'>
-      <FlexContainer fd_c gap='1rem'>
-        <NavLink to={routesV3.RICE_PAGE.route}>
-          <Button secondary>Inicio</Button>
-        </NavLink>
+      <FlexContainer fd_c gap='1rem' jc_fs ai_c>
+        <StyledLink to={routesV3.RICE_PAGE.route}>
+          <FlexContainer gap='0.5rem'>
+            <IoHomeSharp />
+            <Button secondary>Inicio</Button>
+          </FlexContainer>
+        </StyledLink>
         {isAdmin() && (
-          <NavLink to={routesV3.RICE_PAGE.subRoutes.ADMIN_PAGE.route}>
-            <Button secondary>Admin</Button>
-          </NavLink>
+          <FlexContainer gap='0.5rem' jc_fs ai_c>
+            <StyledLink to={routesV3.RICE_PAGE.subRoutes.ADMIN_PAGE.route}>
+              <Button secondary>
+                <IoKeySharp /> Admin
+              </Button>
+            </StyledLink>
+          </FlexContainer>
         )}
       </FlexContainer>
-      <Layout width='90%' mg="1rem 0">
+      <Layout width='90%' mg='1rem 0'>
         <Outlet />
       </Layout>
     </FlexContainer>
   )
 }
+
+const StyledLink = styled(NavLink)`
+  color: ${({ theme: { disableColor } }) => disableColor};
+
+  .activated {
+    opacity: 1;
+    color: ${({ theme: { fontColor } }) => fontColor};
+    border-bottom: 3px solid ${({ theme: { fontColor } }) => fontColor};
+  }
+`
